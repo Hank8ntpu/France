6c63c06caaaa59c320d3cec5918d100eb2189a7b
李俊翰, Lee Jun-Han <s411178060@gm.ntpu.edu.tw>
2024-11-23 15:52:23 +0800

    Update France.py

--- France.py
+++ France.py
@@ -26,11 +26,8 @@ import re
     #else:
      #   logging.error(f"Failed to send Discord notification: {response.status_code}, {response.text}")
 
-def calculate_dates(today_date_str):
-    start_date = datetime(2025, 1, 20)  # 固定起始日期
-    end_date = datetime(2025, 2, 8)    # 固定結束日期
-
-    return start_date.strftime("%Y/%m/%d"), end_date.strftime("%Y/%m/%d")
+start_date = datetime(2025, 1, 20)  # 固定起始日期
+end_date = datetime(2025, 2, 8)    # 固定結束日期
  
 #def calculate_dates(today_date_str):
  #   today = datetime.strptime(today_date_str, "%Y-%m-%d")
