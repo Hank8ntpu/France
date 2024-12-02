92a9239df5a4289fce262609e7b624388180720d
李俊翰, Lee Jun-Han <s411178060@gm.ntpu.edu.tw>
2024-12-02 14:19:55 +0800

    Update France.py

--- France.py
+++ France.py
@@ -26,20 +26,20 @@ import re
     #else:
      #   logging.error(f"Failed to send Discord notification: {response.status_code}, {response.text}")
 
-def calculate_dates(today_date_str):
-    today = datetime.strptime(today_date_str, "%Y-%m-%d")
-    start_date = datetime(2025, 1, 20)
-    end_date = start_date + timedelta(days=(today - datetime(2024, 10, 21)).days)
+# def calculate_dates(today_date_str):
+#    today = datetime.strptime(today_date_str, "%Y-%m-%d")
+#    start_date = datetime(2025, 1, 20)
+#    end_date = start_date + timedelta(days=(today - datetime(2024, 10, 21)).days)
 
     # 如果日期是 2024-12-20 及以後，結束日期固定為 2025-03-21
-    if today >= datetime(2024, 12, 20):
-        end_date = datetime(2025, 3, 21)
+#    if today >= datetime(2024, 12, 20):
+#        end_date = datetime(2025, 3, 21)
 
     # 如果日期是 2025-01-20 及以後，起始日開始遞增
-    if today >= datetime(2025, 1, 20):
-        start_date += timedelta(days=(today - datetime(2025, 1, 20)).days)
+#    if today >= datetime(2025, 1, 20):
+#        start_date += timedelta(days=(today - datetime(2025, 1, 20)).days)
 
-    return start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
+#    return start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
  
 # 設置 Selenium 驅動
 options = Options()
@@ -300,12 +300,14 @@ def scrape_flights(start_date_str, end_date_str):
     return success_count
 
 # 根據當前日期計算動態起始日與結束日
-today_str = datetime.now().strftime("%Y-%m-%d")
-start_date_input, end_date_input = calculate_dates(today_str)
+# today_str = datetime.now().strftime("%Y-%m-%d")
+# start_date_input, end_date_input = calculate_dates(today_str)
 
 try:
     success_count = 0  # 初始化 success_count
     # 調用函式
+    start_date_input = "2025-01-20"  # 固定起始日期
+    end_date_input = "2025-01-21"    # 固定結束日期
     success_count = scrape_flights(start_date_input, end_date_input)
     # 發送成功通知
    # send_discord_notification(f"共抓取 {success_count} 個航班，日期範圍: {start_date_input} 到 {end_date_input}")
